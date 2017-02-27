821d9324b34d64c9a4afe8dc71e2b902027db55b
Krishna Ganeriwal <ganeriwalk@gmail.com>
2017-02-27 23:02:00 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import 'rxjs';
+import App from './components/app';
+import reducers from './reducers';
+
+
+import { createEpicMiddleware } from 'redux-observable';
+import { rootEpic } from './reducers/index';
+const epicMiddleware = createEpicMiddleware(rootEpic);
+
+
+const createStoreWithMiddleware = applyMiddleware(epicMiddleware,ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
