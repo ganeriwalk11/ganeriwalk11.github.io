fa9ebe4e49d6b94a820c39777d2edae43287f446
Krishna Ganeriwal <ganeriwalk@gmail.com>
2017-02-27 22:58:15 +0530

    Create app.js

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import  { Component }  from 'react';
+
+import ActualData from '../containers/ActualData'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ActualData />
+      </div>
+    );
+  }
+}
