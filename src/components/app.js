c1fb32d255cc7729739860dbebaadcf1a556b04d
Krishna Ganeriwal <ganeriwalk@gmail.com>
2017-03-01 19:14:35 +0530

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,13 @@ import React from 'react';
 import  { Component }  from 'react';
 
 import ActualData from '../containers/ActualData'
+import SpreadSheet from '../containers/SpreadSheet'
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <SpreadSheet />
         <ActualData />
       </div>
     );
