6db8e81bb56328c4d22a6921672cbd50d8ae35d2
Krishna Ganeriwal <ganeriwalk@gmail.com>
2017-03-01 19:16:07 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,10 @@ import { combineReducers } from 'redux';
 import { combineEpics } from 'redux-observable';
 import { fetchUserEpic } from '../actions/index';
 import { inputEditEpic } from '../actions/validations';
+import { AddRowEpic } from '../actions/index';
+import { AddColEpic } from '../actions/index';
+import { RunURLEpic } from '../actions/index';
+import { SaveDataEpic } from '../actions/index';
 
 import DataReducer from './reducer_data';
 import Validator from './checkIDReducer';
@@ -13,7 +17,7 @@ const rootReducer = combineReducers({
 });
 
 export const rootEpic = combineEpics(
-  fetchUserEpic,inputEditEpic
+  fetchUserEpic,inputEditEpic,AddRowEpic,AddColEpic,RunURLEpic
 );
 
 export default rootReducer;
